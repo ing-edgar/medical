f75bcc42582e399349dafd81deed0daa649a11ac
Edgar Vega <edgvega@gmail.com>
2022-10-24 15:04:33 -0500

    fixed vite build production

--- resources/js/components/admin/layout/Main.svelte
+++ resources/js/components/admin/layout/Main.svelte
@@ -2,7 +2,7 @@
     import Sidebar from "./Sidebar.svelte";
 </script>
 
-<div class="grid grid-cols-[50px_minmax(900px,_1fr)] h-screen bg-gray-100">
+<div class="grid grid-cols-[65px_minmax(900px,_1fr)] h-screen bg-gray-100">
     <Sidebar />
     <div class="flex-none bg-gray-100 p-4 w-full space-y-2 border">
         <div class="p-2">
