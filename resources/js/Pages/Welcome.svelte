1b89362c3b92f1a2eed0580bb20af1960e1ddd20
Edgar Vega <edgvega@gmail.com>
2022-10-24 10:40:43 -0500

    archivo de configuración producción y timezone en app
    Se asigna un archivo .env para la sección administración y se asigna a la configuración de la aplicación, app.php, el timezone de méxico

--- resources/js/Pages/Welcome.svelte
+++ resources/js/Pages/Welcome.svelte
@@ -3,6 +3,10 @@
     export let title;
 </script>
 
+<svelte:head>
+    <title>Welcome</title>
+</svelte:head>
+
 <Main>
     <div slot="title">Dashboard</div>
     <h1 class="text-red-600">Hola mundo {title}</h1>
